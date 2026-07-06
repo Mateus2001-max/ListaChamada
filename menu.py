78b82f3b1ef45aea6178753831cada57d99a9622
Mateus Rocha <mateusrochalopes12345@gmail.com>
2026-07-06 15:19:50 -0300

    Update menu.py
    Comentando partes onde usam o tkinter

--- menu.py
+++ menu.py
@@ -1,5 +1,5 @@
 from Crud.GerenciadorAlunos import GerenciadorAlunos
-from Interface.Interface_tkinter import  abrir_lista_presenca
+#from Interface.Interface_tkinter import  abrir_lista_presenca
 from datetime import datetime
 from docx import Document
 from docx.shared import Pt
@@ -71,7 +71,8 @@ def exibir_menu():
         elif opcao == "5":
             alunos = ger.listar()
             if alunos:
-                abrir_lista_presenca(alunos)
+                #abrir_lista_presenca(alunos)
+                print("Função de lista de presença via Tkinter desativada no servidor.")
             else:
                 print("Nenhum aluno cadastrado.")
 
